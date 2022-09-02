b226b623e668bed2686183ec128612cd7fe787e6
Vladislav_Chistyakov <vladischis@gmail.com>
2022-09-02 19:54:07 +0500

    A function has been created that calls input when the search button is clicked

--- script/script.js
+++ script/script.js
@@ -1,35 +1,61 @@
-const body = document.body;
-const btnOpenBurger = body.querySelector(".block-burger__btn-burger");
-const burgerBlock = body.querySelector(".block-burger-menu");
-const btnCloseBurger = body.querySelector(".container-top-burger__btn-close");
 
-const btnEther = body.querySelector(".header-botton__ether-btn");
-const blockEther = body.querySelector(".section-header-audio");
+function btnHeader() {
+  const body = document.body;
+  const btnOpenBurger = body.querySelector(".block-burger__btn-burger");
+  const burgerBlock = body.querySelector(".block-burger-menu");
+  const btnCloseBurger = body.querySelector(".container-top-burger__btn-close");
 
 
-btnOpenBurger.addEventListener("click", () => {
-  burgerBlock.classList.add("activ-block");
-})
+  const btnEther = body.querySelector(".header-botton__ether-btn");
+  const blockEther = body.querySelector(".section-header-audio");
 
-btnCloseBurger.addEventListener("click", () => {
-  burgerBlock.classList.remove("activ-block");
-})
+  const btnSearch = body.querySelector(".right-header__search-btn");
+  const inputSearc = body.querySelector(".right-header__input-searc");
 
-btnEther.addEventListener("click", () => {
-  let v = 0;
-  for (let arr of blockEther.classList) {
-    if (arr !== "activ-flex") {
-      v = 0;
-    } else {
-      v = 1;
+  btnOpenBurger.addEventListener("click", () => {
+    burgerBlock.classList.add("activ-block");
+  })
+
+  btnCloseBurger.addEventListener("click", () => {
+    burgerBlock.classList.remove("activ-block");
+  })
+
+  btnEther.addEventListener("click", () => {
+    let status = 0;
+    for (let arr of blockEther.classList) {
+      if (arr !== "activ-flex") {
+        status = 0;
+      } else {
+        status = 1;
+      }
     }
-  }
-  if (v === 1) {
-    blockEther.classList.remove("activ-flex");
-  } else {
-    blockEther.classList.add("activ-flex");
-  }
-})
+    if (status === 1) {
+      blockEther.classList.remove("activ-flex");
+    } else {
+      blockEther.classList.add("activ-flex");
+    }
+  })
+
+  btnSearch.addEventListener("click", () => {
+    if (inputSearc.classList[2] === undefined) {
+      inputSearc.classList.add("right-header__input-searc-js");
+      inputSearc.classList.add("activ-block");
+    } else {
+      inputSearc.classList.remove("right-header__input-searc-js");
+      inputSearc.classList.remove("activ-block");
+    }
+  })
+
+
+}
+
+
+
+btnHeader();
+
+
+
+
 
 
 
